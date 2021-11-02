6d60dd49a28f18f04a8c65c32f3fe59319c605e9
MariiaRachuk <55761894+MariiaRachuk@users.noreply.github.com>
2021-11-02 14:17:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,32 +3,19 @@ import pandas as pd
 import numpy as np
 import plotly_express as px
 import input_sql
-
-# Чекбокс
-# df = pd.read_excel('\отчеты\input1.xlsx', 0)
-# if st.checkbox('Show dataframe'):
-#    st.write(df)
-# Выпадающий список
-
+pivot = (input_sql.table)
 df = st.cache(pd.read_excel)(r'\Users\MRachuk\PycharmProjects\proectExcel\fileTable.xlsx', 0)
-# option = st.selectbox(
-#    'Сегмент боргу?',
-#     df['Сегмент боргу'].unique())
-# 'You selected: ', option
 
-# Выбор нескольких значений
-# options = st.multiselect(
-# 'Сегмент боргу?', df['Сегмент боргу'].unique())
-# st.write('You selected:', options)
-
-clubs = st.sidebar.multiselect('Сегмент боргу?', input_sql.table['Сегмент боргу'].unique())
+clubs = st.sidebar.multiselect('Сегмент боргу?', df['Сегмент боргу'].unique())
 nationalities = st.sidebar.multiselect('К-ть перевірених ІПН по безкоштовній перевірці?',
-                                       input_sql.table['К-ть перевірених ІПН по безкоштовній перевірці'].unique())
+                                       df['К-ть перевірених ІПН по безкоштовній перевірці'].unique())
 # Filter dataframe
-new_df = input_sql.table[(input_sql.table['Сегмент боргу'].isin(clubs))
-                         & (input_sql.table['К-ть перевірених ІПН по безкоштовній перевірці'].isin(nationalities))]
+new_df = df[(df['Сегмент боргу'].isin(clubs))
+                         & (df['К-ть перевірених ІПН по безкоштовній перевірці'].isin(nationalities))]
 # write dataframe to screen
-st.write(new_df)
+st.write(pivot)
+
+st.write(df)
 
 # create figure using plotly express
 fig = px.histogram(new_df, x='Сегмент боргу')
