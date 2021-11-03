d170547ca758a107f944575e4286e62cd3af0a3e
MariiaRachuk <55761894+MariiaRachuk@users.noreply.github.com>
2021-11-03 12:16:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly_express as px
+import plotly.express as px
 import input_sql
 pivot = (input_sql.table)
 df = st.cache(pd.read_excel)(r'\Users\MRachuk\PycharmProjects\proectExcel\fileTable.xlsx', 0)
